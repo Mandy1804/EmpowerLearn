12efac580e4527be7b7f7ba5d0e191ffe659a334
galaxiasssss <vitororbe@gmail.com>
2026-06-28 21:48:22 -0300

    centraliza urls da api no frontend web

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = window.APP_CONFIG.API_BASE_URL;
 const PAGINA_DESTINO = 'daschboard.html';
 
 document.addEventListener('DOMContentLoaded', function () {
